e6ef3549223b64482c84d9e702cc8981417af1d6
Robert Niimi <robertn702@gmail.com>
2016-03-03 07:27:29 -0800

    adds tts

--- order_coffee.py
+++ order_coffee.py
@@ -1,12 +1,17 @@
 """Orders Coffee"""
 from datetime import datetime
+import re
 import requests
+import subprocess
 
 from order_ahead import OrderAhead
 from credentials import IFTTT_MAKER_KEY, ORDER_AHEAD_USERNAME, ORDER_AHEAD_PASSWORD
 
 IFTTT_TRIGGER_URL = 'https://maker.ifttt.com/trigger/order_coffee/with/key/'
 IFTTT_MAKER_URL = IFTTT_TRIGGER_URL + IFTTT_MAKER_KEY
+
+def say(say_this):
+    subprocess.call(['espeak', say_this])
 
 def expense_order(order, now):
     data = {
@@ -22,22 +27,36 @@ def order_coffee():
     """orders coffee"""
     now = datetime.today()
     if now.hour > 14:
-        print "it's too late for coffee..."
+        message = "it's too late for coffee..."
+        say(message)
+        print message
         return
 
     # log in to order ahead
     order_ahead_client = OrderAhead(ORDER_AHEAD_USERNAME, ORDER_AHEAD_PASSWORD)
 
     if order_ahead_client.has_current_orders():
-        print 'coffee already on order...'
+        message = 'coffee already on order...'
+        say(message)
+        print message
         return
 
     print 'ordering coffee...'
     order_response = order_ahead_client.order()
     print order_response['message']
 
-    # log expense if it is a weekday
-    if order_response['success'] and now.weekday() < 5:
-        print 'logging order to spreadsheet...'
+    if order_response['success']:
         order = order_response['order']
-        expense_order(order, now)
+        formatted_time = re.sub(r"-08:00", "", order['ready_time'])
+        parsed_time = datetime.strptime(formatted_time, "%Y-%m-%dT%H:%M:%S")
+        message = "Your coffee will be ready at {0}:{1}{2}".format(
+            parsed_time.hour if parsed_time.hour <= 12 else parsed_time.hour - 12,
+            parsed_time.minute,
+            'AM' if parsed_time.hour < 12 else 'PM'
+            )
+        say(message)
+
+        # log expense if it is a weekday
+        if now.weekday() < 5:
+            print 'logging order to spreadsheet...'
+            expense_order(order, now)
---
+++ test_datetime.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+import subprocess
+
+subprocess.call(['espeak', 'hello'])
