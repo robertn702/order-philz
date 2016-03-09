4b453e668ee758f3ad5f781fa54a0d0cbcf2d9b6
Robert Niimi <robertn702@gmail.com>
2016-03-08 20:46:50 -0800

    adds spoken leave time

--- order_coffee.py
+++ order_coffee.py
@@ -1,5 +1,5 @@
 """Orders Coffee"""
-from datetime import datetime
+from datetime import datetime, timedelta
 import re
 import subprocess
 import requests
@@ -12,6 +12,11 @@ IFTTT_MAKER_URL = IFTTT_TRIGGER_URL + IFTTT_MAKER_KEY
 
 def say(say_this):
     subprocess.call(['say', say_this])
+
+def say_at(say_this, say_time):
+    echo_say = subprocess.Popen(['echo', "say '{0}'".format(say_this)], stdout=subprocess.PIPE)
+    subprocess.call(('at', say_time), stdin=echo_say.stdout)
+    echo_say.wait()
 
 def expense_order(order, now):
     data = {
@@ -52,12 +57,18 @@ def order_coffee():
         order = order_response['order']
         formatted_time = re.sub(r"-08:00", "", order['ready_time'])
         parsed_time = datetime.strptime(formatted_time, "%Y-%m-%dT%H:%M:%S")
+        ready_hour = parsed_time.hour if parsed_time.hour <= 12 else parsed_time.hour - 12
+        ready_minute = parsed_time.minute
+        ready_suffix = 'AM' if parsed_time.hour < 12 else 'PM'
         message = "Your coffee will be ready at {0}:{1}{2}".format(
-            parsed_time.hour if parsed_time.hour <= 12 else parsed_time.hour - 12,
-            parsed_time.minute,
-            'AM' if parsed_time.hour < 12 else 'PM'
-            )
+            ready_hour,
+            ready_minute,
+            ready_suffix
+        )
         say(message)
+
+        leave_time = parsed_time - timedelta(minutes=5)
+        say_at("Time to pick up your coffee", "{0}:{1}".format(leave_time.hour, leave_time.minute))
 
         # log expense if it is a weekday
         if now.weekday() < 5:
